d269131c20430c0d628e95f9300e3eb846eb326c
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-09-05 12:09:42 +0300

    init

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import Nav from '../components/Nav';
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <head><script>{(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-ABCDEFGH')}</script></head>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-ABCDEFGH" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <Nav />
       <Component {...pageProps} />
     </>
